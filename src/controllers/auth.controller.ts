7b41df253812c438699c48ec9225674added8c5b
Nuril Anwar Al-Absory <nurilanwaralabsory@gmail.com>
2025-04-06 14:25:19 +0700

    API auth register

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+type TRegister = {
+  fullName: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+export default {
+  register(req: Request, res: Response) {
+    const { fullName, username, email, password, confirmPassword } =
+      req.body as unknown as TRegister;
+  },
+};
